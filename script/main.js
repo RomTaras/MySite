112723c8dbb2a6b9b472756afff044b0d640f1bf
Taras <tarik.register@gmail.com>
2017-04-24 15:32:32 +0300

    3 commit

--- script/main.js
+++ script/main.js
@@ -2,19 +2,20 @@ var imgHead = [
             'img/1.jpg',
             'img/2.jpg',
             'img/3.jpg',
-            'img/4.jpg'
+            'img/4.jpg',
+            'img/5.jpg'
         ], i=1;
     function csaHead(){
  
         if(i > (imgHead.length-1)){
             $('.csa-head').animate({'opacity':'0'},200,function(){
                 i=1;
-                $('.csa-head').css({'background':'url('+imgHead[0]+')'});
+                $('.csa-head').css({'background-image':'url('+imgHead[0]+')'});
             });
             $('.csa-head').animate({'opacity':'1'},200);
         }else{
             $('.csa-head').animate({'opacity':'0'},200,function(){
-                $('.csa-head').css({'background':'url('+imgHead[i]+')'});
+                $('.csa-head').css({'background-image':'url('+imgHead[i]+')'});
                 i++;
             });
             $('.csa-head').animate({'opacity':'1'},200);
